973679ec28238742e97b24e7a3d25248f5418425
sparvasterino <sparvasterino@gmail.com>
2021-02-22 19:14:52 +0000

    Added Code via repl

---
+++ script.js
@@ -0,0 +1,44 @@
+
+//let teleport = function(){
+ // if()
+
+
+
+//}
+
+
+
+let lamaimg = {
+  topValue: 50,
+  rightValue: 50,
+
+  moveUp: function () {
+    lamaimg.topValue -= 5;
+    $("#LamaImage").css("top", lamaimg.topValue + "%");
+  },
+
+  moveDown: function () {
+    lamaimg.topValue += 5;
+    $("#LamaImage").css("top", lamaimg.topValue + "%");
+  },
+
+  moveRight: function () {
+    lamaimg.rightValue -= 5;
+    $("#LamaImage").css("right", lamaimg.rightValue + "%");
+  },
+
+  moveLeft: function () {
+    lamaimg.rightValue += 5;
+    $("#LamaImage").css("right", lamaimg.rightValue + "%");
+  },
+}
+
+let init = function () {
+
+  $("#moveUp").on("click", lamaimg.moveUp);
+  $("#moveDown").on("click", lamaimg.moveDown);
+  $("#moveRight").on("click", lamaimg.moveRight);
+  $("#moveLeft").on("click", lamaimg.moveLeft);
+};
+
+$(document).ready(init);
